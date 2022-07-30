aosp-ginkgo/packages_apps_Settings

Settings: add support for changing icon pack
Change-Id: Iddeb57419fa967e001c8628b6ef1af42b9731c60

src/com/android/settings/DisplaySettings.java
@@ -97,6 +97,8 @@ public class DisplaySettings extends DashboardFragment {
                 "android.theme.customization.font"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.adaptive_icon_shape"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.icon_pack"));
         return controllers;
     }
 

manikantaraavi/packages_apps_Settings-1

Signed-off-by: DennySPb <dennyspb@gmail.com>
Signed-off-by: manikantaraavi <raavimanikanta6595@gmail.com>

src/com/android/settings/DisplaySettings.java
@@ -87,6 +87,8 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.font"));
         return controllers;
     }
 